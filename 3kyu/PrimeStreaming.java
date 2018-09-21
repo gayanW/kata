5cb6a1383ed673e6dfdd7cc5487022f8d320c526
Gayan Weerakutti <gayan@linuxdeveloper.space>
2018-09-21 18:07:49 +0530

    Update kata: Prime Streaming

--- 3kyu/PrimeStreaming.java
+++ 3kyu/PrimeStreaming.java
@@ -7,18 +7,35 @@ import java.util.stream.IntStream;
  */
 public class PrimeStreaming {
 
-  private static final int LIMIT = 100000000;
-  private static boolean[] eliminated = new boolean[LIMIT + 1];
-
   public static IntStream stream() {
-    final int RANGE = (int) Math.sqrt(LIMIT);
-    for (int i = 2; i <= RANGE; i++) {
-      if (!eliminated[i]) {
-        for (int eliminate = i * 2; eliminate <= LIMIT; eliminate += i)
-          eliminated[eliminate] = true;
-      }
-    }
-    return IntStream.range(2, LIMIT).filter(i -> !eliminated[i]);
+    return IntStream.range(2, Integer.MAX_VALUE).filter(PrimeStreaming::isPrime);
   }
+
+  private static boolean isPrime(int n) {
+    return IntStream.rangeClosed(2, (int) Math.sqrt(n)).noneMatch(i -> n % i == 0);
+  }
+
+  /**
+   * (((((((((( Sieve of Eratosthenes approach ))))))))))
+   */
+
+  static class Sieve {
+
+    private static final int LIMIT = Integer.MAX_VALUE - 1;
+    private static boolean[] eliminated = new boolean[LIMIT + 1];
+
+    public static IntStream streamSieve() {
+      final int RANGE = (int) Math.sqrt(LIMIT);
+      for (int i = 2; i <= RANGE; i++) {
+        if (!eliminated[i]) {
+          for (int eliminate = i * 2; eliminate <= LIMIT; eliminate += i)
+            eliminated[eliminate] = true;
+        }
+      }
+      return IntStream.range(2, LIMIT).filter(i -> !eliminated[i]);
+    }
+
+  }
+
 }
 
